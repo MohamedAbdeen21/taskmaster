8d1edfd98f588f7dfd2608789c4ebd2168ecc81f
Mohamed Abdeen <mohamedabden21@gmail.com>
2024-01-22 10:31:41 +0200

    Handle SIGINT during task run

--- src/dag/executor.py
+++ src/dag/executor.py
@@ -1,5 +1,9 @@
+from multiprocessing.spawn import is_forking
+import os
 import time
+import signal
 from multiprocessing import Process
+from daemonize import Daemonize, handlers
 from datetime import datetime, timezone
 
 # When PyO3 executes Python code, it has to 
@@ -11,6 +15,10 @@ from datetime import datetime, timezone
 class Executor:
     def __init__(self):
         self.graphs = []
+        self.handlers = []
+        self.pid = os.getpid()
+        signal.signal(signal.SIGINT, self.wait)
+
     def add(self, graph):
         next = graph.next()
         for (idx,(dt, graphs)) in enumerate(self.graphs):
@@ -18,12 +26,25 @@ class Executor:
                 graphs.append(graph)
                 return
             if dt < next:
-                self.graphs.insert(idx, [graph])
+                self.graphs.insert(idx, (next,[graph]))
                 return
 
         self.graphs.append((next, [graph]))
 
+    def wait(self, signum, frame):
+        # Signals and handlers are passed to children by default
+        if self.pid != os.getpid():
+            return
+
+        print("Got ctrl-c, waiting for tasks")
+        while any([handler.is_alive() for handler in self.handlers]):
+            time.sleep(1)
+
+        exit()
+
+
     def start(self):
+        print(os.getpid())
         while True:
             (next, graphs) = self.graphs.pop()
 
@@ -32,9 +53,14 @@ class Executor:
 
             delta = next - now 
 
-            time.sleep(delta.total_seconds())
-            handlers = [Process(target=graph.start) for graph in graphs]
+            time.sleep(max(0, delta.total_seconds()))
+            self.handlers = [Process(target=graph.start) for graph in graphs]
 
             [self.add(graph) for graph in graphs]
-            [handler.start() for handler in handlers]
-            [handler.join() for handler in handlers]
+            [handler.start() for handler in self.handlers]
+            [handler.join() for handler in self.handlers]
+            self.handlers.clear()
+
+        # pid = "./tm.pid"
+        # daemon = Daemonize(app="tm", pid=pid, action=self.loop)
+        # daemon.start()
